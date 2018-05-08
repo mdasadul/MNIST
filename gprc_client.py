f9fa59e780e7b36af7b264c2a6c9e02bd4ead990
mdasadul <mdasadul@ualberta.ca>
2018-05-08 15:21:21 -0400

    adding gprc client

---
+++ gprc_client.py
@@ -0,0 +1,50 @@
+import numpy as np
+import requests 
+import json
+from tensorflow.examples.tutorials.mnist import input_data
+import argparse
+from gprc.beta import implementations
+import tensorflow as tf
+
+from tensorflow_serving.apis import predict_pb2, prediction_service_pb2
+from tensorflow.contrib.util import make_tensor_proto
+
+
+def main(args):  
+    
+    host, port = args.host, args.port
+
+    mnist = input_data.read_data_sets(args.data_dir, one_hot=True)
+
+    channel = implementations.insecure_channel(host, int(port))
+    stub = prediction_service_pb2.beta_create_PredictionService_server(channel)
+
+    counter = 0
+    num_tests = args.num_tests
+    for _ in range(num_tests):
+        image, label = mnist.test.next_batch(1)
+        
+        request = predict_pb2.PredictRequest()
+        request.model_spec.name = 'mnist'
+        request.model_spec.signature_name = tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY
+        request.inputs['x_input'].CopyFrom(
+            make_tensor_proto(image[0],image[0].size)
+        )
+        result_future = stub.Predict.future(request,10.0)
+        response = result_future.result().outputs['y_output']
+
+        if np.argmax(response) ==np.argmax(label):
+            counter +=1
+        else:
+            pass
+    print("Accuracy= %0.2f"%((counter*1.0/num_tests)*100))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', default='/tmp/data')
+    parser.add_argument('--host',default='http://127.0.0.1')
+    parser.add_argument('--port',default=5000)
+    parser.add_argument('--num_tests',default=100)
+    args = parser.parse_args()
+
+    main(args)
